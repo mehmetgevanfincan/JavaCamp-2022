d900247de8cfb23adb9bbbebfddb601422b3519c
mehmetgevanfincan <m.gevanfincan@gmail.com>
2022-10-23 21:03:20 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
+
+        //değişken isimlendirmeleri javada camelCase yazılır.
+        String ortaMetin ="İlginizi çekebilir";
+        String altMetin ="Vade Süresi";
+
+        System.out.println(ortaMetin);
+        System.out.println(altMetin);
+
+        //int deger
+        int vade = 12;
+
+        double dolarDun = 18.15;
+        double dolarBugun = 18.20;
+
+        boolean dolarDustuMu = false;
+
+        String okYonu = "";
+
+        if (dolarBugun<dolarDun) {
+            okYonu = "down.svg";
+            System.out.println(okYonu);
+        }else if(dolarBugun>dolarDun) {
+            okYonu = "up.svg";
+            System.out.println(okYonu);
+        }
+        else {
+            okYonu = "equal.svg";
+            System.out.println(okYonu);
+        }
+
+        //array
+        String[] krediler = {"Hızlı Kredi","Maaşını Halkbanktan","Mutlu Emekli"};
+
+
+        for (int i = 0; i < krediler.length; i++) {
+            System.out.println(krediler[i]);
+        }
+
     }
+
 }
